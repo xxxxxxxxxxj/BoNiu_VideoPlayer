4282ccc8470a8226ba098c68e30224daf123640a
xujun <xvjun@haotang365.com.cn>
2020-03-25 21:41:57 +0800

    RecyclerView优化

--- app/src/main/java/com/boniu/shipinbofangqi/mvp/view/adapter/ChooseVideoAdapter.java
+++ app/src/main/java/com/boniu/shipinbofangqi/mvp/view/adapter/ChooseVideoAdapter.java
@@ -1,18 +1,9 @@
 package com.boniu.shipinbofangqi.mvp.view.adapter;
 
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.boniu.shipinbofangqi.R;
-import com.boniu.shipinbofangqi.util.CommonUtil;
-import com.boniu.shipinbofangqi.util.FileSizeUtil;
-import com.boniu.shipinbofangqi.util.GlideUtil;
-import com.boniu.shipinbofangqi.util.StringUtil;
+import com.boniu.shipinbofangqi.mvp.view.widget.VideoGrid;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.duyin.quickscan.baen.ScanResult;
-import com.zhihu.matisse.internal.ui.widget.CheckView;
 
 import java.util.List;
 
@@ -25,25 +16,14 @@ import java.util.List;
  * @date zhoujunxia on 2020-03-22 16:24
  */
 public class ChooseVideoAdapter extends BaseQuickAdapter<ScanResult, BaseViewHolder> {
-    private int width;
-
-    public ChooseVideoAdapter(int layoutResId, List<ScanResult> data, int width) {
+    public ChooseVideoAdapter(int layoutResId, List<ScanResult> data) {
         super(layoutResId, data);
-        this.width = width;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, ScanResult item) {
-        CheckView check_view = helper.getView(R.id.check_view);
-        ImageView media_thumbnail = helper.getView(R.id.media_thumbnail);
-        TextView video_duration = helper.getView(R.id.video_duration);
-        if (item != null) {
-            check_view.setChecked(item.isSelect());
-            int videoDuration = CommonUtil.getLocalVideoDuration(item.getPath());
-            String formatVideoDuration = FileSizeUtil.formatSeconds(videoDuration / 1000);
-            StringUtil.setText(video_duration, formatVideoDuration, "", View.VISIBLE, View.GONE);
-            GlideUtil.displayVideoCoverImg(mContext, item.getPath(), media_thumbnail);
-        }
+        VideoGrid videoGrid = (VideoGrid) helper.itemView;
+        videoGrid.bindMedia(item);
     }
 }
 
---
+++ app/src/main/java/com/boniu/shipinbofangqi/mvp/view/widget/VideoGrid.java
@@ -0,0 +1,58 @@
+package com.boniu.shipinbofangqi.mvp.view.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.boniu.shipinbofangqi.R;
+import com.boniu.shipinbofangqi.util.CommonUtil;
+import com.boniu.shipinbofangqi.util.FileSizeUtil;
+import com.boniu.shipinbofangqi.util.GlideUtil;
+import com.boniu.shipinbofangqi.util.StringUtil;
+import com.duyin.quickscan.baen.ScanResult;
+import com.zhihu.matisse.internal.ui.widget.CheckView;
+import com.zhihu.matisse.internal.ui.widget.SquareFrameLayout;
+
+/**
+ * <p>Title:${type_name}</p>
+ * <p>Description:</p>
+ * <p>Company:北京昊唐科技有限公司</p>
+ *
+ * @author 徐俊
+ * @date zhoujunxia on 2020-03-25 21:27
+ */
+public class VideoGrid extends SquareFrameLayout {
+    private ImageView mThumbnail;
+    private CheckView mCheckView;
+    private ImageView mGifTag;
+    private TextView mVideoDuration;
+
+    public VideoGrid(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public VideoGrid(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.media_grid_content, this, true);
+        mThumbnail = (ImageView) findViewById(R.id.media_thumbnail);
+        mCheckView = (CheckView) findViewById(R.id.check_view);
+        mGifTag = (ImageView) findViewById(R.id.gif);
+        mVideoDuration = (TextView) findViewById(R.id.video_duration);
+    }
+
+    public void bindMedia(ScanResult item) {
+        mCheckView.setChecked(item.isSelect());
+        int videoDuration = CommonUtil.getLocalVideoDuration(item.getPath());
+        String formatVideoDuration = FileSizeUtil.formatSeconds(videoDuration / 1000);
+        StringUtil.setText(mVideoDuration, formatVideoDuration, "", View.VISIBLE, View.GONE);
+        GlideUtil.displayVideoCoverImg(getContext(), item.getPath(), mThumbnail);
+    }
+}
